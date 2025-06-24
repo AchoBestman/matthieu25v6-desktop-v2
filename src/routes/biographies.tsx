c2bdf7c1242e552843fbb3e5d58e433e8c53f525
AchileAikpe <achile.aikpe@akieni.com>
2025-06-24 16:23:48 +0100

    feat: implement biography route with data fetching and rendering; add Biography schema

--- src/routes/biographies.tsx
+++ src/routes/biographies.tsx
@@ -1,3 +1,10 @@
+import { useLangue } from "@/context/langue-context";
+import { useSermon } from "@/context/sermon-context";
+import { useTheme } from "@/context/theme-context";
+import { resources } from "@/lib/resources";
+import { findBy } from "@/lib/resources/biography";
+import { tr } from "@/translation";
+import { useQuery } from "@tanstack/react-query";
 import { createFileRoute } from "@tanstack/react-router";
 
 export const Route = createFileRoute("/biographies")({
@@ -5,5 +12,48 @@ export const Route = createFileRoute("/biographies")({
 });
 
 function RouteComponent() {
-  return <div>Hello "/biographies"!</div>;
+  const { lng } = useLangue();
+  const { theme } = useTheme();
+  const { fontSize } = useSermon();
+
+  const {
+    data: biography,
+    isLoading,
+    isError,
+  } = useQuery({
+    queryKey: ["biographies", lng],
+    queryFn: () => findBy(resources.biographies, lng),
+  });
+
+  return (
+    <div className="mt-1">
+      {isLoading && tr("home.waiting")}
+      {isError && tr("home.search_not_found_pred_message")}
+      {biography && (
+        <div>
+          <div className="w-full">
+            <img
+              alt="background"
+              className="float-left pr-4 mt-2"
+              width={200}
+              height={200}
+              src={`/images/photo-prophete.jpg`}
+            />
+          </div>
+          <div
+            style={{ fontSize }}
+            dangerouslySetInnerHTML={{
+              __html: biography.description
+                .replace(
+                  /<(h1|h2)>/g,
+                  `<strong style="color:${theme === "dark" ? "orange" : "#7b3d1a"} !important; font-size: 22px;"><$1>`
+                )
+                .replace(/<\/(h1|h2)>/g, "</strong></$1>"),
+            }}
+            className="text-justify w-11.5/12 mt-4 sm:mt-0 sm:w-full"
+          ></div>
+        </div>
+      )}
+    </div>
+  );
 }
