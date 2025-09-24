1ca07de807212fe961a9b021ca51650a6de03ac7
AIKPE ACHILE <aikpeachille55@gmail.com>
2025-09-24 21:39:30 +0000

    update local db

--- src/lib/env.ts
+++ src/lib/env.ts
@@ -1,5 +1,5 @@
 export const REMOTE_SERVER = "https://api.philippekacou.org";
-//export const REMOTE_SERVER = "https://matthieu25v6-api-v2.test";
+//  export const REMOTE_SERVER = "https://matthieu25v6-api-v2.test";
 export const API_URL = `${REMOTE_SERVER}/api`;
 
 //download historique local_key
